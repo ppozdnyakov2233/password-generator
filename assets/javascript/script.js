3c7c24b5bee5b2058e014bee577fb6271e361a15
ppozdnyakov <rpozdnyakov@hotmail.com>
2022-01-16 15:38:39 -0800

    added lines

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,13 @@
 // Assignment code here and Event Listener
 
-document.querySelector("#generate").addEventListener("click,writePassword");
+document.querySelector("#generate").addEventListener("click",writePassword);
 
 
 // Arrays
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChar = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?', '~'];
 var alphaLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var alphaLower = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var alphaUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
 // Var declarations
@@ -71,7 +71,21 @@ function generatePassword() {
 
   console.log(passwordCharacters)
 
+  var randomPassword = ""
+
+
+  for (var i = 0; i < confirmLength; i++) {
+      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      console.log(randomPassword)
+  }
+
+  return randomPassword;
+
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
