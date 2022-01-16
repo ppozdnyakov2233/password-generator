ecae00fcb8f6390218774252ab5f07b7d5958012
ppozdnyakov <rpozdnyakov@hotmail.com>
2022-01-16 14:32:56 -0800

    added arrays

---
+++ assets/javascript/script.js
@@ -0,0 +1,23 @@
+// Assignment code here and Event Listener
+
+document.querySelector("#generate").addEventListener("click,writePassword");
+
+
+// Arrays
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?', '~'];
+var alphaLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphaLower = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+
+
+// Write password to the #password input
+function generatePassword() {
+  
+  
+  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
